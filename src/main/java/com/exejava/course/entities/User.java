9b6457950bcf6a94165570e5efdc85cf4da2f521
Rafael Brendo <rafalive122@hotmail.com>
2023-01-16 11:00:49 -0300

    H2 database, test profile, JPA.

--- src/main/java/com/exejava/course/entities/User.java
+++ src/main/java/com/exejava/course/entities/User.java
@@ -1,18 +1,26 @@
 package com.exejava.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public class User implements Serializable{
-	
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+
 	
 	public User() {
 	}
@@ -68,7 +76,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -80,7 +91,13 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
+	
+	
 }
--- src/main/java/com/exejava/course/resources/UserResource.java
+++ src/main/java/com/exejava/course/resources/UserResource.java
@@ -7,17 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.exejava.course.entities.User;
 
-//Isso significa que essa classe é um recurso web que é implementado por um controlador
-//rest
-@RestController //Anotetion
-@RequestMapping(value = "/users") //fiz uma anotetion com um recurso. Um recurso relacionado a entidade User
+
+@RestController 
+@RequestMapping(value = "/users") 
 public class UserResource {
 	
-	@GetMapping //Para indicar que esse metodo vai ser o metodo que responde a requisição do tipo get do http
+	@GetMapping 
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com","9999999", "12345");
 		return ResponseEntity.ok().body(u);
-				//.ok -> para retornar a resposta com sucesso no http
-				//vou retornar o 'corpo' da resposta para o meu usuario u.
+				
 	}
 }
