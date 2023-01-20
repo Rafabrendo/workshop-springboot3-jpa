31b1d83aace1000cb09d36ca35b173dbdaad27fa
Rafael Brendo <rafalive122@hotmail.com>
2023-01-20 11:28:01 -0300

    Exception handling - update

--- src/main/java/com/exejava/course/services/UserService.java
+++ src/main/java/com/exejava/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.exejava.course.repositories.UserRepository;
 import com.exejava.course.resources.exceptions.DatabaseException;
 import com.exejava.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service //É uma classe de serviço então dei preferencia ao @Service .
 public class UserService {
 	
@@ -46,9 +48,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateDate(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	//Não vou atualizar o id e nem o password
